526fc42ac13a071b22f244324c6ef66fe48c83c6
neel <neelrai906@gmail.com>
2021-09-24 13:01:02 +0530

    functions-defined

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let inputArr = process.argv.slice(2);
-console.log(inputArr);
+// console.log(inputArr);
 
 let command = inputArr[0];
 switch(command){
     case "tree":
+        treeFn(inputArr[1])
         break;
     case "organise":
+        organizeFn(input[1]);
         break;
     case "help":
+        helpFn();
         break;
     default:
         console.log("Input the write command");
@@ -19,13 +22,16 @@ switch(command){
 function treeFn(dirPath){
     console.log("Tree command implemented for",dirPath);
 }
-function treeFn(dirPath){
-    console.log("organize command implemented for")
+function organizeFn(dirPath){
+    console.log("organize command implemented for",dirPath);
 }
-function treeFn(dirPath){
-}
-unction treeFn(dirPath){
-
+function helpFn(){
+    console.log(`
+    List of All the commands
+        node main.js tree "directory"
+        node main.js organize "directoryPath"
+        node main.js help
+    `);
 }
 
 
