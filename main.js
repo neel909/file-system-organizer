66b99ad83e6ece1bbcff2eb4862c755f0bbabad1
neel <neelrai906@gmail.com>
2021-09-24 10:12:14 +0530

    input-node-and-function-psuedo-code

---
+++ main.js
@@ -0,0 +1,31 @@
+let inputArr = process.argv.slice(2);
+console.log(inputArr);
+
+let command = inputArr[0];
+switch(command){
+    case "tree":
+        break;
+    case "organise":
+        break;
+    case "help":
+        break;
+    default:
+        console.log("Input the write command");
+        break;
+
+
+}
+
+function treeFn(dirPath){
+    console.log("Tree command implemented for",dirPath);
+}
+function treeFn(dirPath){
+    console.log("organize command implemented for")
+}
+function treeFn(dirPath){
+}
+unction treeFn(dirPath){
+
+}
+
+
